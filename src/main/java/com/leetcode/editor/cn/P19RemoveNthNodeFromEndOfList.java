d37b2c33c5f7704441147ad5faa6cf1dfdf8e7a5
Gouzhong1223 <1162864960@qq.com>
2020-11-04 18:08:42 +0800

    新增 020 有效的括号

--- src/main/java/com/leetcode/editor/cn/P19RemoveNthNodeFromEndOfList.java
+++ src/main/java/com/leetcode/editor/cn/P19RemoveNthNodeFromEndOfList.java
@@ -36,17 +36,36 @@ public class P19RemoveNthNodeFromEndOfList {
             ListNode pre = new ListNode(0);
             pre.next = head;
             ListNode start = pre, end = pre;
+            // start 节点先向前移动 N 位
             while (n != 0) {
                 start = start.next;
                 n--;
             }
+            // start 和 end同时移动，直到 start.next 为 null 的时候，此时，start 和 end 相差 N 位，这时的 end 刚好就是倒数第 N 位
             while (start.next != null) {
                 start = start.next;
                 end = end.next;
             }
+            // 将 end 的 next 节点直接删除==>倒数第 N 位的节点被删除
             end.next = end.next.next;
+            // 返回 pre.next 的而不返回 head 的原因是因为，head 可能就是被删除的节点
             return pre.next;
         }
+//
+//        public static void main(String[] args) {
+//            ListNode listNode = new ListNode(1);
+//            ListNode listNode1 = new ListNode(2);
+//            listNode.next = listNode1;
+//            ListNode listNode2 = new ListNode(3);
+//            listNode1.next = listNode2;
+//            ListNode listNode3 = new ListNode(4);
+//            listNode2.next = listNode3;
+//            ListNode listNode4 = new ListNode(5);
+//            listNode3.next = listNode4;
+//            Solution solution = new Solution();
+//            System.out.println(solution.removeNthFromEnd(listNode, 2).val);
+//
+//        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
---
+++ src/main/java/com/leetcode/editor/cn/P21MergeTwoSortedLists.java
@@ -0,0 +1,34 @@
+package com.leetcode.editor.cn;
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//
+//
+// 示例：
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+//
+// Related Topics 链表
+// 👍 1356 👎 0
+
+public class P21MergeTwoSortedLists {
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            return null;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
