4ec68473325610d066d265adf8369150bfb8a20f
Gouzhong1223 <1162864960@qq.com>
2020-11-10 20:25:31 +0800

    新增 31 下一个排列
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P31NextPermutation.java
+++ src/main/java/com/leetcode/editor/cn/P31NextPermutation.java
@@ -14,16 +14,63 @@ package com.leetcode.editor.cn;
 
 public class P31NextPermutation {
     //leetcode submit region begin(Prohibit modification and deletion)
-    static class Solution {
+    class Solution {
         public void nextPermutation(int[] nums) {
-            
+            if (nums == null || nums.length == 0) {
+                return;
+            }
+            // 找出第一个指针，满足 nums[k]>nums[k+1]
+            int firstIndex = -1;
+            for (int i = nums.length - 2; i >= 0; i--) {
+                if (nums[i] < nums[i + 1]) {
+                    firstIndex = i;
+                    break;
+                }
+            }
+            // 若没有找到 k 指针,则说明数组为降序数组，此时将数组进行翻转-->升序
+            if (firstIndex == -1) {
+                reverse(nums, 0, nums.length - 1);
+                return;
+            }
+            int secondIndex = -1;
+            for (int i = nums.length - 1; i >= 0; i--) {
+                if (nums[i] > nums[firstIndex]) {
+                    secondIndex = i;
+                    break;
+                }
+            }
+            swap(nums, firstIndex, secondIndex);
+            reverse(nums, firstIndex + 1, nums.length - 1);
         }
 
-        public static void main(String[] args) {
-            Solution solution = new Solution();
-            solution.nextPermutation(new int[]{1, 2, 3, 4, 5});
+        /**
+         * 翻转数组中从 i->j 之间的元素
+         *
+         * @param nums 被翻转的数组
+         * @param i    左指针
+         * @param j    右指针
+         */
+        private void reverse(int[] nums, int i, int j) {
+            // 左右指针不断往中间夹逼，完成交换的步骤
+            while (i < j) {
+                swap(nums, i++, j--);
+            }
+        }
+
+        /**
+         * 交换数组中指定下标的两个元素
+         *
+         * @param nums 被交换的数组
+         * @param i    需要交换的元素 1
+         * @param i1   需要交换的元素 2
+         */
+        private void swap(int[] nums, int i, int i1) {
+            int tmp = nums[i];
+            nums[i] = nums[i1];
+            nums[i1] = tmp;
         }
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
