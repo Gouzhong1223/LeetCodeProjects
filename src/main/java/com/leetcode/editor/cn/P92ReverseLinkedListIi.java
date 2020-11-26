34ce1e0ff073c90f1068aabd0fa50a4eb474b284
Gouzhong1223 <1162864960@qq.com>
2020-11-26 21:07:45 +0800

    添加 092 反转链表从 m 到 n 的元素
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

---
+++ src/main/java/com/leetcode/editor/cn/P92ReverseLinkedListIi.java
@@ -0,0 +1,56 @@
+package com.leetcode.editor.cn;
+//反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+//
+// 说明:
+//1 ≤ m ≤ n ≤ 链表长度。
+//
+// 示例:
+//
+// 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+//输出: 1->4->3->2->5->NULL
+// Related Topics 链表
+// 👍 586 👎 0
+
+public class P92ReverseLinkedListIi {
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+
+        // 后驱节点
+        ListNode successor = null;
+
+        public ListNode reverseBetween(ListNode head, int m, int n) {
+            if (m == 1) {
+                return reverseN(head, n);
+            }
+            head.next = reverseBetween(head.next, m - 1, n - 1);
+            return head;
+
+        }
+
+        public ListNode reverseN(ListNode head, int n) {
+            if (n == 1) {
+                // 记录第 n + 1 个节点
+                successor = head.next;
+                return head;
+            }
+            // 以 head.next 为起点，需要反转前 n - 1 个节点
+            ListNode last = reverseN(head.next, n - 1);
+
+            head.next.next = head;
+            // 让反转之后的 head 节点和后面的节点连起来
+            head.next = successor;
+            return last;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
