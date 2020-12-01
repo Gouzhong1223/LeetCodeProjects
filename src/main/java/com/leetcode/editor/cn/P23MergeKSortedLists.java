491f28a888716f13421daa1f865a8350ecd04184
Gouzhong1223 <1162864960@qq.com>
2020-12-01 14:01:42 +0800

    create->新增 023 合并 K 个升序列表
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P23MergeKSortedLists.java
+++ src/main/java/com/leetcode/editor/cn/P23MergeKSortedLists.java
@@ -59,26 +59,70 @@ public class P23MergeKSortedLists {
      * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
-    class Solution {
+    static class Solution {
         public ListNode mergeKLists(ListNode[] lists) {
-            return null;
+            if (lists == null || lists.length == 0) {
+                return null;
+            }
+            return merage(lists, 0, lists.length - 1);
+
         }
 
-        public class ListNode {
-            int val;
-            ListNode next;
-
-            ListNode() {
+        public ListNode merage(ListNode[] lists, int left, int right) {
+            if (left == right) {
+                return lists[left];
             }
+            int mid = (left + right) / 2;
+            ListNode l = merage(lists, left, mid);
+            ListNode r = merage(lists, mid + 1, right);
+            return merageTwo(l, r);
+        }
 
-            ListNode(int val) {
-                this.val = val;
+        /**
+         * 合并两个链表
+         *
+         * @param l1
+         * @param l2
+         * @return
+         */
+        public ListNode merageTwo(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
             }
+            if (l2 == null) {
+                return l1;
+            }
+            if (l1.val < l2.val) {
+                l1.next = merageTwo(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = merageTwo(l1, l2.next);
+                return l2;
+            }
+        }
 
-            ListNode(int val, ListNode next) {
-                this.val = val;
-                this.next = next;
-            }
+
+        public static void main(String[] args) {
+            Solution solution = new Solution();
+            ListNode[] listNodes = new ListNode[3];
+            ListNode listNode = new ListNode(1);
+            ListNode listNode2 = new ListNode(4);
+            listNode.next = listNode2;
+            listNode2.next = new ListNode(5);
+
+            ListNode listNode4 = new ListNode(1);
+            ListNode listNode5 = new ListNode(3);
+            listNode4.next = listNode5;
+            listNode5.next = new ListNode(4);
+
+            ListNode listNode7 = new ListNode(2);
+            listNode7.next = new ListNode(6);
+            listNodes[0] = listNode;
+            listNodes[1] = listNode4;
+            listNodes[2] = listNode7;
+
+            ListNode listNode1 = solution.mergeKLists(listNodes);
+
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
