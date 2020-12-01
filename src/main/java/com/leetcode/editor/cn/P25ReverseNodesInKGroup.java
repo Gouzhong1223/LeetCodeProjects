abe81070e094c6b502fe25dcd9bd0e5f94afe61f
Gouzhong1223 <1162864960@qq.com>
2020-12-01 15:48:17 +0800

    create->新增 025 K 个一组反转链表
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P25ReverseNodesInKGroup.java
+++ src/main/java/com/leetcode/editor/cn/P25ReverseNodesInKGroup.java
@@ -40,10 +40,83 @@ public class P25ReverseNodesInKGroup {
      * }
      */
     class Solution {
-        public ListNode reverseKGroup(ListNode head, int k) {
 
-            return null;
+        ListNode successor = null;
+
+        public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode pre = new ListNode(0);
+            pre.next = head;
+            int counter = 0;
+            while (pre.next != null) {
+                counter += 1;
+                pre = pre.next;
+            }
+            if (k == 0 || counter < k) {
+                return head;
+            }
+            for (int i = 1; counter >= k; i = i + k, counter -= k) {
+                head = reverseMN(head, i, i + k - 1);
+            }
+            return head;
         }
+
+        /**
+         * 反转链表的 M 到 N
+         *
+         * @param head 链表
+         * @param m    M
+         * @param n    N
+         * @return 反转之后的链表
+         */
+        public ListNode reverseMN(ListNode head, int m, int n) {
+            if (m == 1) {
+                return reverseN(head, n);
+            }
+            head.next = reverseMN(head.next, m - 1, n - 1);
+            successor = null;
+            return head;
+        }
+
+        /**
+         * 反转链表前 N 个元素
+         *
+         * @param head 链表
+         * @param n    N
+         * @return 反转之后的链表
+         */
+        public ListNode reverseN(ListNode head, int n) {
+            if (n == 1) {
+                successor = head.next;
+                return head;
+            }
+            ListNode last = reverseN(head.next, n - 1);
+            head.next.next = head;
+            head.next = successor;
+            return last;
+        }
+
+//        public static void main(String[] args) {
+//            Solution solution = new Solution();
+//            ListNode listNode = new ListNode(1);
+//            ListNode listNode1 = new ListNode(2);
+//            ListNode listNode2 = new ListNode(3);
+//            ListNode listNode3 = new ListNode(4);
+//            ListNode listNode4 = new ListNode(5);
+//            ListNode listNode5 = new ListNode(6);
+//            ListNode listNode6 = new ListNode(7);
+//            ListNode listNode7 = new ListNode(8);
+//            listNode.next = listNode1;
+//            listNode1.next = listNode2;
+//            listNode2.next = listNode3;
+//            listNode3.next = listNode4;
+//            listNode4.next = listNode5;
+//            listNode5.next = listNode6;
+//            listNode6.next = listNode7;
+//            ListNode listNode8 = solution.reverseKGroup(listNode, 3);
+//            for (ListNode l1 = listNode8; l1 != null; l1 = l1.next) {
+//                System.out.println(l1.val);
+//            }
+//        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
