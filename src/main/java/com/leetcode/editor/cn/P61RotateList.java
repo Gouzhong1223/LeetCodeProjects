c2b01bc3f17f979887d82837e0a0704914f703b3
Gouzhong1223 <1162864960@qq.com>
2020-12-01 22:23:22 +0800

    create->新增 061 旋转链表
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P61RotateList.java
+++ src/main/java/com/leetcode/editor/cn/P61RotateList.java
@@ -33,9 +33,86 @@ public class P61RotateList {
      * ListNode(int x) { val = x; }
      * }
      */
-    class Solution {
+    static class Solution {
         public ListNode rotateRight(ListNode head, int k) {
-            return null;
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode oldTail = head;
+            int n;
+            for(n = 1; oldTail.next != null; n++) {
+                oldTail = oldTail.next;
+            }
+            oldTail.next = head;
+            ListNode newTail = head;
+            for (int i = 0; i < n - k % n - 1; i++) {
+                newTail = newTail.next;
+            }
+            ListNode newHead = newTail.next;
+            newTail.next = null;
+            return newHead;
+
+
+//            // 统计节点个数
+//            int num = 1;
+//            ListNode cur = head;
+//            while (cur.next != null) {
+//                cur = cur.next;
+//                num++;
+//            }
+//            // 记录尾节点
+//            ListNode tail = cur;
+//
+//            // 是否会变回原位置
+//            int count = k % num;
+//            if (count == 0) {
+//                return head;
+//            }
+//
+//            // 找到需要移动的节点中最后一个
+//            count = num - count;
+//            cur = head;
+//            while (count > 1) {
+//                cur = cur.next;
+//                count--;
+//            }
+//
+//            // 重构链表
+//            ListNode next = cur.next;
+//            cur.next = null;
+//            tail.next = head;
+//            return next;
+        }
+
+//        /**
+//         * 将链表进行一次旋转
+//         *
+//         * @param head 链表
+//         */
+//        public void rotate(ListNode head) {
+//            ListNode last = head;
+//            while (last.next != null && last.next.next != null) {
+//                ListNode n = last.next;
+//                n.next = null;
+//                last = last.next.next;
+//            }
+//            last.next = head;
+//        }
+
+        public static void main(String[] args) {
+            Solution solution = new Solution();
+            ListNode listNode = new ListNode(0);
+            ListNode listNode1 = new ListNode(1);
+            ListNode listNode2 = new ListNode(2);
+            ListNode listNode3 = new ListNode(3);
+            listNode.next = listNode1;
+            listNode1.next = listNode2;
+            listNode2.next = listNode3;
+            ListNode listNode4 = solution.rotateRight(listNode, 1);
+            while (listNode4 != null) {
+                System.out.println(listNode4.val);
+                listNode4 = listNode4.next;
+            }
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
