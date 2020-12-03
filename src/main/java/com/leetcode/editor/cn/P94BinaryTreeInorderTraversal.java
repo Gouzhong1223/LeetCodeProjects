9321293166225f1acfa32f9c3f5894df5002c02c
Gouzhong1223 <1162864960@qq.com>
2020-12-03 21:00:47 +0800

    create->新增 094 二叉树中序遍历
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P94BinaryTreeInorderTraversal.java
+++ src/main/java/com/leetcode/editor/cn/P94BinaryTreeInorderTraversal.java
@@ -55,6 +55,7 @@ package com.leetcode.editor.cn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class P94BinaryTreeInorderTraversal {
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -78,7 +79,17 @@ public class P94BinaryTreeInorderTraversal {
 
         public List<Integer> inorderTraversal(TreeNode root) {
             List<Integer> res = new ArrayList<>();
-            inOrder(root, res);
+            Stack<TreeNode> stack = new Stack<>();
+            while (root != null || !stack.isEmpty()) {
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                res.add(root.val);
+                root = root.right;
+            }
+//            inOrder(root, res);
             return res;
         }
 
