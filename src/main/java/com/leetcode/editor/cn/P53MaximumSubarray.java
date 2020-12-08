d46c77435bd9c948ca14bfa9df8ae08bba625819
Gouzhong1223 <1162864960@qq.com>
2020-12-08 13:01:17 +0800

    create->新增 053 最大子序和
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

---
+++ src/main/java/com/leetcode/editor/cn/P53MaximumSubarray.java
@@ -0,0 +1,36 @@
+package com.leetcode.editor.cn;
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//
+// 示例:
+//
+// 输入: [-2,1,-3,4,-1,2,1,-5,4]
+//输出: 6
+//解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+//
+//
+// 进阶:
+//
+// 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+// Related Topics 数组 分治算法 动态规划
+// 👍 2692 👎 0
+
+public class P53MaximumSubarray {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int ans = nums[0];
+            int sum = 0;
+            for (int num : nums) {
+                if (sum > 0) {
+                    sum += num;
+                } else {
+                    sum = num;
+                }
+                ans = Math.max(ans, sum);
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
