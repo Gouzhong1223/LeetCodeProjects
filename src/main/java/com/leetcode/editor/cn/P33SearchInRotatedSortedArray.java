e9ea981d6dad8700ffc892daebbecd24d9bf88e2
Gouzhong1223 <1162864960@qq.com>
2020-12-08 21:59:01 +0800

    create->新增 033 搜索旋转排序数组
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

---
+++ src/main/java/com/leetcode/editor/cn/P33SearchInRotatedSortedArray.java
@@ -0,0 +1,75 @@
+package com.leetcode.editor.cn;
+//给你一个整数数组 nums ，和一个整数 target 。
+//
+// 该整数数组原本是按升序排列，但输入时在预先未知的某个点上进行了旋转。（例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2]
+// ）。
+//
+// 请你在数组中搜索 target ，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [4,5,6,7,0,1,2], target = 0
+//输出：4
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [4,5,6,7,0,1,2], target = 3
+//输出：-1
+//
+// 示例 3：
+//
+//
+//输入：nums = [1], target = 0
+//输出：-1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 5000
+// -10^4 <= nums[i] <= 10^4
+// nums 中的每个值都 独一无二
+// nums 肯定会在某个点上旋转
+// -10^4 <= target <= 10^4
+//
+// Related Topics 数组 二分查找
+// 👍 1085 👎 0
+
+public class P33SearchInRotatedSortedArray {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int search(int[] nums, int target) {
+            int low = 0;
+            int high = nums.length - 1;
+            int mid;
+            while (low <= high) {
+                mid = low + (high - low) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[mid] >= nums[low]) {
+                    if (target >= nums[low] && target < nums[mid]) {
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+                } else {
+                    if (target <= nums[high] && target > nums[mid]) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
