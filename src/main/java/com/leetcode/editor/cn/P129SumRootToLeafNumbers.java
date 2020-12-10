47d7b7fabcde0ea06f7042342beae7cf06b4c5cd
Gouzhong1223 <1162864960@qq.com>
2020-12-10 23:37:29 +0800

    create->新增 0129 求根节点到叶子节点数字之和
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

---
+++ src/main/java/com/leetcode/editor/cn/P129SumRootToLeafNumbers.java
@@ -0,0 +1,71 @@
+package com.leetcode.editor.cn;
+//给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+//
+// 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+//
+// 计算从根到叶子节点生成的所有数字之和。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例 1:
+//
+// 输入: [1,2,3]
+//    1
+//   / \
+//  2   3
+//输出: 25
+//解释:
+//从根到叶子节点路径 1->2 代表数字 12.
+//从根到叶子节点路径 1->3 代表数字 13.
+//因此，数字总和 = 12 + 13 = 25.
+//
+// 示例 2:
+//
+// 输入: [4,9,0,5,1]
+//    4
+//   / \
+//  9   0
+// / \
+//5   1
+//输出: 1026
+//解释:
+//从根到叶子节点路径 4->9->5 代表数字 495.
+//从根到叶子节点路径 4->9->1 代表数字 491.
+//从根到叶子节点路径 4->0 代表数字 40.
+//因此，数字总和 = 495 + 491 + 40 = 1026.
+// Related Topics 树 深度优先搜索
+// 👍 293 👎 0
+
+public class P129SumRootToLeafNumbers {
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode root, int prevNumber) {
+            if (root == null) {
+                return 0;
+            }
+            int sum = prevNumber * 10 + root.val;
+            if (root.left == null && root.right == null) {
+                return sum;
+            } else {
+                return dfs(root.left, sum) + dfs(root.right, sum);
+            }
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
