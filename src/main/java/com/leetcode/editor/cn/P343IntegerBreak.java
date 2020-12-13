674d0910beb4ba75e7ebd849a5436e84a2caee56
Gouzhong1223 <1162864960@qq.com>
2020-12-13 12:24:29 +0800

    create->新增 0343 整数拆分
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

---
+++ src/main/java/com/leetcode/editor/cn/P343IntegerBreak.java
@@ -0,0 +1,40 @@
+package com.leetcode.editor.cn;
+//给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+//
+// 示例 1:
+//
+// 输入: 2
+//输出: 1
+//解释: 2 = 1 + 1, 1 × 1 = 1。
+//
+// 示例 2:
+//
+// 输入: 10
+//输出: 36
+//解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+//
+// 说明: 你可以假设 n 不小于 2 且不大于 58。
+// Related Topics 数学 动态规划
+// 👍 415 👎 0
+
+public class P343IntegerBreak {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int integerBreak(int n) {
+            if (n <= 3) {
+                return n - 1;
+            }
+            int a = n / 3, b = n % 3;
+            if (b == 0) {
+                return (int) Math.pow(3, a);
+            }
+            if (b == 1) {
+                return (int) Math.pow(3, a - 1) * 4;
+            }
+            return (int) Math.pow(3, a) * 2;
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
