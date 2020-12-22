a127744a43e8c6b4fb9c4eb7b97e61f465dbb455
Gouzhong1223 <1162864960@qq.com>
2020-12-22 12:22:19 +0800

    create->新增 064 最小路径和
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P64MinimumPathSum.java
+++ src/main/java/com/leetcode/editor/cn/P64MinimumPathSum.java
@@ -37,7 +37,19 @@ public class P64MinimumPathSum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int minPathSum(int[][] grid) {
-            return 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (i == 0 && j == 0) {
+                    } else if (i == 0) {
+                        grid[i][j] = grid[i][j - 1] + grid[i][j];
+                    } else if (j == 0) {
+                        grid[i][j] = grid[i - 1][j] + grid[i][j];
+                    } else {
+                        grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                    }
+                }
+            }
+            return grid[grid.length - 1][grid[0].length - 1];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
