0d5491d973356b44756d031c11fea8e395a21c64
Gouzhong1223 <1162864960@qq.com>
2020-12-25 22:07:16 +0800

    create->新增 01115 顺序打印 foobar
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P1115PrintFoobarAlternately.java
+++ src/main/java/com/leetcode/editor/cn/P1115PrintFoobarAlternately.java
@@ -40,6 +40,8 @@ package com.leetcode.editor.cn;
 //
 // 👍 80 👎 0
 
+import java.util.concurrent.Semaphore;
+
 public class P1115PrintFoobarAlternately {
     //leetcode submit region begin(Prohibit modification and deletion)
     class FooBar {
@@ -49,21 +51,26 @@ public class P1115PrintFoobarAlternately {
             this.n = n;
         }
 
+        Semaphore foo = new Semaphore(1);
+        Semaphore bar = new Semaphore(0);
+
         public void foo(Runnable printFoo) throws InterruptedException {
 
             for (int i = 0; i < n; i++) {
-
+                foo.acquire();
                 // printFoo.run() outputs "foo". Do not change or remove this line.
                 printFoo.run();
+                bar.release();
             }
         }
 
         public void bar(Runnable printBar) throws InterruptedException {
 
             for (int i = 0; i < n; i++) {
-
+                bar.acquire();
                 // printBar.run() outputs "bar". Do not change or remove this line.
                 printBar.run();
+                foo.release();
             }
         }
     }
