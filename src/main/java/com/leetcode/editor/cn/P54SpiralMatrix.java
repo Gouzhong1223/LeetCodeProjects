335194e97b89db90c4f84e568014cd152c95b0ba
Gouzhong1223 <1162864960@qq.com>
2020-12-28 13:30:39 +0800

    create->新增 054 旋转矩阵
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P54SpiralMatrix.java
+++ src/main/java/com/leetcode/editor/cn/P54SpiralMatrix.java
@@ -33,28 +33,58 @@ public class P54SpiralMatrix {
     class Solution {
         public List<Integer> spiralOrder(int[][] matrix) {
             List<Integer> res = new ArrayList<>();
-            if (matrix.length == 0) {
+            if (matrix.length == 0 || matrix[0].length == 0) {
                 return res;
             }
-            int m = matrix.length;
-            int n = matrix[0].length;
-            int i = 0;
-            int count = (Math.min(m, n) + 1) / 2;
-            while (i < count) {
-                for (int j = i; j < n - i; j++) {
-                    res.add(matrix[i][j]);
+            int left = 0, right = matrix[0].length - 1, top = 0, bottom = matrix.length - 1;
+            while (true) {
+                for (int i = left; i <= right; i++) {
+                    res.add(matrix[top][i]);
                 }
-                for (int j = i + 1; j < m - i; j++) {
-                    res.add(matrix[j][(n - 1) - i]);
+                if (++top > bottom) {
+                    break;
                 }
-                for (int j = (n - 1) - (i + 1); j >= i && (m - 1 - i != i); j--) {
-                    res.add(matrix[m - 1 - i][j]);
+                for (int i = top; i <= bottom; i++) {
+                    res.add(matrix[i][right]);
                 }
-                for (int j = (m - 1) - (i + 1); j >= i + 1 && ((n - 1 - i) != i); j--) {
-                    res.add(matrix[j][i]);
+                if (--right < left) {
+                    break;
                 }
-                i++;
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                if (--bottom < top) {
+                    break;
+                }
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                if (++left > right) {
+                    break;
+                }
             }
+            //            if (matrix.length == 0) {
+//                return res;
+//            }
+//            int m = matrix.length;
+//            int n = matrix[0].length;
+//            int i = 0;
+//            int count = (Math.min(m, n) + 1) / 2;
+//            while (i < count) {
+//                for (int j = i; j < n - i; j++) {
+//                    res.add(matrix[i][j]);
+//                }
+//                for (int j = i + 1; j < m - i; j++) {
+//                    res.add(matrix[j][(n - 1) - i]);
+//                }
+//                for (int j = (n - 1) - (i + 1); j >= i && (m - 1 - i != i); j--) {
+//                    res.add(matrix[m - 1 - i][j]);
+//                }
+//                for (int j = (m - 1) - (i + 1); j >= i + 1 && ((n - 1 - i) != i); j--) {
+//                    res.add(matrix[j][i]);
+//                }
+//                i++;
+//            }
             return res;
         }
     }
