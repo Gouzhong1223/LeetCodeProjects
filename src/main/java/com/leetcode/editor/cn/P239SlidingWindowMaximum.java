5b64251defada4396d0d63c0bd5501047d4249d0
Gouzhong1223 <1162864960@qq.com>
2021-01-02 15:57:01 +0800

    create->新增 0239 滑动窗口最大值
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P239SlidingWindowMaximum.java
+++ src/main/java/com/leetcode/editor/cn/P239SlidingWindowMaximum.java
@@ -39,11 +39,30 @@ package com.leetcode.editor.cn;
 // Related Topics 堆 Sliding Window
 // 👍 663 👎 0
 
+import java.util.LinkedList;
+
 public class P239SlidingWindowMaximum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] maxSlidingWindow(int[] nums, int k) {
-            return new int[]{};
+            if (nums == null || nums.length < 2) {
+                return nums;
+            }
+            LinkedList<Integer> queue = new LinkedList<>();
+            int[] res = new int[nums.length - k + 1];
+            for (int i = 0; i < nums.length; i++) {
+                while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+                    queue.pollLast();
+                }
+                queue.addLast(i);
+                if (queue.peek() <= i - k) {
+                    queue.poll();
+                }
+                if (i + 1 >= k) {
+                    res[i + 1 - k] = nums[queue.peek()];
+                }
+            }
+            return res;
         }
 
     }
