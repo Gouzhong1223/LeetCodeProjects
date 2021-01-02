4ed8df4d233eced46b0b223e01a0e128aa8b0006
Gouzhong1223 <1162864960@qq.com>
2021-01-02 16:18:50 +0800

    create->新增 0208 实现 Trie（前缀树）
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P208ImplementTriePrefixTree.java
+++ src/main/java/com/leetcode/editor/cn/P208ImplementTriePrefixTree.java
@@ -24,33 +24,65 @@ package com.leetcode.editor.cn;
 public class P208ImplementTriePrefixTree {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Trie {
+        private TrieNode root;
 
         /**
          * Initialize your data structure here.
          */
         public Trie() {
-
+            root = new TrieNode();
         }
 
         /**
          * Inserts a word into the trie.
          */
         public void insert(String word) {
-
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                if (node.next[c - 'a'] == null) {
+                    node.next[c - 'a'] = new TrieNode();
+                }
+                node = node.next[c - 'a'];
+            }
+            node.isEnd = true;
         }
 
         /**
          * Returns if the word is in the trie.
          */
         public boolean search(String word) {
-            return true;
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                node = node.next[c - 'a'];
+                if (node == null) {
+                    return false;
+                }
+            }
+            return node.isEnd;
         }
 
         /**
          * Returns if there is any word in the trie that starts with the given prefix.
          */
         public boolean startsWith(String prefix) {
+            TrieNode node = root;
+            for (char c : prefix.toCharArray()) {
+                node = node.next[c - 'a'];
+                if (node == null) {
+                    return false;
+                }
+            }
             return true;
+        }
+
+        class TrieNode {
+            private boolean isEnd;
+            TrieNode[] next;
+
+            public TrieNode() {
+                this.isEnd = false;
+                next = new TrieNode[26];
+            }
         }
     }
 
