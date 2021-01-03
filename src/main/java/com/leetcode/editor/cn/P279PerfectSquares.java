f448725f9be193f06c6b2e9041121cb9246a1ae1
Gouzhong1223 <1162864960@qq.com>
2021-01-03 22:45:05 +0800

    create->新增 0279 完全平方数
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

---
+++ src/main/java/com/leetcode/editor/cn/P279PerfectSquares.java
@@ -0,0 +1,27 @@
+package com.leetcode.editor.cn;
+//给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+//
+// 示例 1:
+//
+// 输入: n = 12
+//输出: 3
+//解释: 12 = 4 + 4 + 4.
+//
+// 示例 2:
+//
+// 输入: n = 13
+//输出: 2
+//解释: 13 = 4 + 9.
+// Related Topics 广度优先搜索 数学 动态规划
+// 👍 719 👎 0
+
+public class P279PerfectSquares {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numSquares(int n) {
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
