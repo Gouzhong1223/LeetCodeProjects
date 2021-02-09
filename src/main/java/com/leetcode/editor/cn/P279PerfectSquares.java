401a485d282c9460688ff4b12fd72da1b236d68c
Gouzhong1223 <1162864960@qq.com>
2021-02-09 22:45:03 +0800

    create->新增 0279 完全平方数
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P279PerfectSquares.java
+++ src/main/java/com/leetcode/editor/cn/P279PerfectSquares.java
@@ -19,7 +19,14 @@ public class P279PerfectSquares {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int numSquares(int n) {
-            return 0;
+            int[] dp = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                dp[i] = i;
+                for (int j = 1; i - j * j >= 0; j++) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+            return dp[n];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
