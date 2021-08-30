974d3e0fa3f14e1c903d4a716efdcecb6143b147
Gouzhong1223 <1162864960@qq.com>
2021-08-30 22:10:44 +0800

    0005 最长回文子串
    Signed-off-by: Gouzhong1223 <1162864960@qq.com>

--- src/main/java/com/leetcode/editor/cn/P5LongestPalindromicSubstring.java
+++ src/main/java/com/leetcode/editor/cn/P5LongestPalindromicSubstring.java
@@ -20,36 +20,68 @@ public class P5LongestPalindromicSubstring {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public String longestPalindrome(String s) {
-            int length = s.length();
-            if (length < 2) {
-                return s;
+            if (s == null || s.length() == 0) {
+                return "";
             }
-            boolean[][] dp = new boolean[length][length];
-            for (int i = 0; i < length; i++) {
-                dp[i][i] = true;
-            }
-
-            int maxlength = 1;
-            int begin = 0;
-            char[] chars = s.toCharArray();
-            for (int j = 1; j < length; j++) {
-                for (int i = 0; i < j; i++) {
-                    if (chars[i] != chars[j]) {
-                        dp[i][j] = false;
-                    } else {
-                        if (j - i < 3) {
-                            dp[i][j] = true;
-                        } else {
-                            dp[i][j] = dp[i + 1][j - 1];
-                        }
-                    }
-                    if (dp[i][j] && j - i + 1 > maxlength) {
-                        maxlength = j - i + 1;
-                        begin = i;
-                    }
+            int strLen = s.length();
+            int left;
+            int right;
+            int len = 1;
+            int maxStart = 0;
+            int maxLen = 0;
+            for (int i = 0; i < strLen; i++) {
+                left = i - 1;
+                right = i + 1;
+                while (left >= 0 && s.charAt(left) == s.charAt(i)) {
+                    len++;
+                    left--;
                 }
+                while (right < strLen && s.charAt(right) == s.charAt(i)) {
+                    len++;
+                    right++;
+                }
+                while (left >= 0 && right < strLen && s.charAt(right) == s.charAt(left)) {
+                    len += 2;
+                    left--;
+                    right++;
+                }
+                if (len > maxLen) {
+                    maxLen = len;
+                    maxStart = left;
+                }
+                len = 1;
             }
-            return s.substring(begin, begin + maxlength);
+            return s.substring(maxStart + 1, maxStart + maxLen + 1);
+//            int length = s.length();
+//            if (length < 2) {
+//                return s;
+//            }
+//            boolean[][] dp = new boolean[length][length];
+//            for (int i = 0; i < length; i++) {
+//                dp[i][i] = true;
+//            }
+//
+//            int maxlength = 1;
+//            int begin = 0;
+//            char[] chars = s.toCharArray();
+//            for (int j = 1; j < length; j++) {
+//                for (int i = 0; i < j; i++) {
+//                    if (chars[i] != chars[j]) {
+//                        dp[i][j] = false;
+//                    } else {
+//                        if (j - i < 3) {
+//                            dp[i][j] = true;
+//                        } else {
+//                            dp[i][j] = dp[i + 1][j - 1];
+//                        }
+//                    }
+//                    if (dp[i][j] && j - i + 1 > maxlength) {
+//                        maxlength = j - i + 1;
+//                        begin = i;
+//                    }
+//                }
+//            }
+//            return s.substring(begin, begin + maxlength);
 
 
 //            int maxLen = 1;
